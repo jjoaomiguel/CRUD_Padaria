2768dd012e1e8b8e6477caafc3c343c7fe4d54dd
João Medeiros <joao_m_medeiros@estudante.sesisenai.org.br>
2025-08-12 12:42:20 -0300

    add - adicionando formulario no create

--- create.php
+++ create.php
@@ -1,3 +1,27 @@
+<?php
+
+include 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $sql = " INSERT INTO usuarios (name,email) VALUE ('$name','$email')";
+
+    if ($conn->query($sql) === true) {
+        echo "Novo registro criado com sucesso.";
+    } else {
+        echo "Erro " . $sql . '<br>' . $conn->error;
+    }
+    $conn->close();
+}
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -17,5 +41,22 @@
         </div>
 
     </header>
+
+    <form method="POST" action="create.php">
+
+        <label for="name">Produtos:</label>
+        <input type="text" name="name" required>
+
+        <input type="submit" value="Buscar">
+
+        <label for="name">Categorias:</label>
+        <input type="text" name="name" required>
+
+        <input type="submit" value="Buscar">
+
+    </form>
+
+    <a href="read.php">Ver registros.</a>
+
 </body>
 </html>
