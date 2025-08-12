a993bd964837c797a5c9dbd8d044f1273fe3858d
João Medeiros <joao_m_medeiros@estudante.sesisenai.org.br>
2025-08-12 11:15:36 -0300

    add - index iniciado

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Bumba Meu Pão</title>
+</head>
+<body>
+     <header>
+
+        <div id="navbar">
+            <img id="icone" src="imagens/logo.png" alt="Logo Bumba Meu Pão">
+        </div>
+
+    </header>
+</body>
+</html>
