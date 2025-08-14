8a473efc782e840f26291a24af5ed33d7bf437b4
Hisabel Lippel <hisabel_lippel@estudante.sesisenai.org.br>
2025-08-14 09:02:46 -0300

    add - adicionar compras do cliente

---
+++ script/cliente_read.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Document</title>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+    <div class="card" style="width: 18rem;">
+      <svg aria-label="Espaço reservado: tampa da imagem" class="bd-placeholder-img card-img-top" height="180" preserveAspectRatio="xMidYMid slice" role="img" width="100%" xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
+      <div class="card-body">
+        <h5 class="card-title"><font dir="auto" style="vertical-align: inherit;"><font dir="auto" style="vertical-align: inherit;">Croassaint</font></font></h5>
+        <p class="card-text"><font dir="auto" style="vertical-align: inherit;"><font dir="auto" style="vertical-align: inherit;"><strong>R$ 7,99</strong>   Um crossaint tamanho médio, fazemos na hora, para uma pessoa.</font></font></p>
+        <a href="#" class="btn btn-primary"><font dir="auto" style="vertical-align: inherit;"><font dir="auto" style="vertical-align: inherit;">Adicionar ao carrinho</font></font></a>
+      </div>
+    </div>
+  </body>
+</html>
