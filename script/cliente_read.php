d3f60e8333188cfd3e40dadd55745dd1f858cda9
Hisabel Lippel <hisabel_lippel@estudante.sesisenai.org.br>
2025-08-14 10:09:01 -0300

    add - carrinho

--- script/cliente_read.php
+++ script/cliente_read.php
@@ -4,10 +4,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" href="../style/media.css">
+    <link rel="stylesheet" href="../style/style.css">
     <title>Document</title>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 
 </head>
+<header>
+
+        <div class="navbar">
+            <div class="flex">
+                <img class="icone" src="../assets/logo.png" alt="Logo Bumba Meu Pão">
+            </div>
+        </div>
+
+</header>
+
+
 <body class="p-3 m-0 border-0 bd-example m-0 border-0">
     <div class="card" style="width: 18rem;">
         <img src="../assets/croassaint_imagem.jpg" class="card-img-top" alt="Crsoissant">
