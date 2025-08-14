c2d83c5ea77fd3b2d57e33db46ec984027660fa5
Hisabel Lippel <hisabel_lippel@estudante.sesisenai.org.br>
2025-08-14 08:51:05 -0300

    add - botões no index

--- index.php
+++ index.php
@@ -3,22 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/media.css">
-    <link rel="stylesheet" href="style/style.css">
-    <a href="script/produto_create.php"></a>
-    <a href="script/db.php"></a>
-    <a href="script/delete.php"></a>
-    <a href="script/produto_read.php"></a>
-    <a href="script/produto_update.php"></a>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" href="../style/media.css">
+    <link rel="stylesheet" href="../style/style.css">
+    <a href="../script/produto_create.php"></a>
+    <a href="../script/db.php"></a>
+    <a href="../script/delete.php"></a>
+    <a href="../script/produto_read.php"></a>
+    <a href="../script/produto_update.php"></a>
     <title>Bumba Meu Pão</title>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body>
-     <a href="script/produto_create.php">Gerente</a>
-     <a href="script/cliente_read.php">Cliente</a>
-
-    <div class="d-grid gap-2">
-        <a href="script/produto_create.php"><button class="btn btn-primary" type="button">Gerente</button></a>
-        <button class="btn btn-primary" type="button">Cliente</button>
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+    <div class="d-grid gap-2 col-6 mx-auto">
+      <button class="btn btn-primary" type="button"><font dir="auto" style="vertical-align: inherit;"><font dir="auto" style="vertical-align: inherit;">Gerente</font></font></button>
+      <button class="btn btn-primary" type="button"><font dir="auto" style="vertical-align: inherit;"><font dir="auto" style="vertical-align: inherit;">Cliente</font></font></button>
     </div>
-</body>
+  </body>
 </html>
